15cec86b0b19edc29ac816f3cc4e5c2609770ec0
User <limasthays@gmail.com>
2023-09-11 14:40:07 -0300

    sets tailwind config and global style settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			'deep-blue': '#212859',
+			'off-white': '#D5CDB3',
+			'very-deep-blue': '#151A3D',
+			'blush-pink': '#F08CAE',
+		},
+	},
+	plugins: [],
 }
 export default config
