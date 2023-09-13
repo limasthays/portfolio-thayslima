81748c2cc8cd02fafa45101ef6f1aae3965afca2
User <limasthays@gmail.com>
2023-09-13 15:22:50 -0300

    projects cards template

---
+++ src/app/components/containers/ProjectCard.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+import Image from 'next/image'
+import TriangleOpen from '../../assets/icons/triangle-open.svg'
+import TriangleClose from '../../assets/icons/triangle-close.svg'
+import { useState } from 'react'
+
+type LinkProps = {
+	title: string
+	link: string
+}
+
+type ProjectCardProps = {
+	img: string | StaticImport
+	projectTags: string
+	title: string
+	techs: string
+	links: LinkProps[]
+	about: string
+}
+
+function ProjectCard({
+	img,
+	projectTags,
+	title,
+	techs,
+	links,
+	about,
+}: ProjectCardProps) {
+	const [isTextVisible, setIsTextVisible] = useState<boolean>(false)
+
+	return (
+		<div className="flex flex-col items-center">
+			<Image src={img} alt="project mockup" className="max-w-xs" />
+			<div className="text-center">
+				<p className="font-light text-xs">{projectTags}</p>
+				<h3 className="uppercase font-bold">{title}</h3>
+				<p className="font-light text-sm">{techs}</p>
+			</div>
+
+			<div className="flex gap-2 my-2">
+				{links.map((item) => (
+					<button
+						title={item.title}
+						key={item.title}
+						className="border border-deep-blue px-2 py-1 font-bold text-xs"
+					>
+						<a href={item.link}>{item.title}</a>
+					</button>
+				))}
+
+				<button
+					onClick={() => {
+						setIsTextVisible(!isTextVisible)
+					}}
+					className="flex items-center border border-deep-blue px-2 py-1 font-bold text-xs"
+				>
+					sobre o app
+					<Image
+						src={isTextVisible ? TriangleClose : TriangleOpen}
+						alt="triangle button icon"
+						className="h-2 ml-1"
+					/>
+				</button>
+			</div>
+
+			{isTextVisible && <p className="text-sm my-1">{about}</p>}
+		</div>
+	)
+}
+
+export default ProjectCard
