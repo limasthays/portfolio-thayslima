147d0e94368078352cdf5a5a45ccfc2d06401d50
User <limasthays@gmail.com>
2023-09-13 16:24:57 -0300

    adds white variant prop to sections components

--- src/app/components/containers/SectionContainer.tsx
+++ src/app/components/containers/SectionContainer.tsx
@@ -1,19 +1,29 @@
+import clsx from 'clsx'
 import SectionTitle from '../headings/SectionTitle'
 
 type SectionContainerProps = {
 	children: React.ReactNode
 	id: string
 	sectionTitle: string
+	whiteVariant?: boolean
 }
 
 function SectionContainer({
 	children,
 	id,
 	sectionTitle,
+	whiteVariant,
 }: SectionContainerProps) {
 	return (
-		<section className="min-h-screen px-6" id={id}>
-			<SectionTitle title={sectionTitle} />
+		<section
+			className={clsx(
+				`min-h-screen px-6 py-2 ${
+					whiteVariant && 'bg-off-white text-deep-blue'
+				}`
+			)}
+			id={id}
+		>
+			<SectionTitle title={sectionTitle} whiteVariant={whiteVariant} />
 			{children}
 		</section>
 	)
--- src/app/components/headings/SectionTitle.tsx
+++ src/app/components/headings/SectionTitle.tsx
@@ -1,8 +1,24 @@
-function SectionTitle({ title }: { title: string }) {
+import clsx from 'clsx'
+
+type SectionTitleProps = { title: string; whiteVariant?: boolean }
+
+function SectionTitle({ title, whiteVariant }: SectionTitleProps) {
 	return (
-		<div className="flex gap-2 items-center sticky top-0 py-6">
+		<div
+			className={clsx(
+				`flex gap-2 items-center sticky top-0 py-4 ${
+					whiteVariant ? 'bg-off-white' : 'bg-deep-blue'
+				}`
+			)}
+		>
 			<h2 className="uppercase font-bold text-xl min-w-fit">{title}</h2>
-			<hr className="w-full border-[1px] border-deep-blue" />
+			<hr
+				className={clsx(
+					`w-full border-[1px] ${
+						whiteVariant ? 'border-deep-blue' : 'border-off-white'
+					}`
+				)}
+			/>
 		</div>
 	)
 }
