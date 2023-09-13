b624671c5d48c67f2d34aeedeaaea7a0b7b93763
User <limasthays@gmail.com>
2023-09-13 15:23:20 -0300

    includes projects content on its cards and renders on projects section

--- src/app/components/sections/Projects.tsx
+++ src/app/components/sections/Projects.tsx
@@ -1,9 +1,24 @@
 import SectionContainer from '../containers/SectionContainer'
+import { projectsContent } from '@/app/content/projectsContent'
+import ProjectCard from '../containers/ProjectCard'
+import { nanoid } from 'nanoid'
 
 function ProjectsSection() {
 	return (
 		<SectionContainer id="projects" sectionTitle="projetos">
-			<div></div>
+			<div>
+				{projectsContent.map((item) => (
+					<ProjectCard
+						img={item.img}
+						projectTags={item.projectTags}
+						title={item.title}
+						techs={item.techs}
+						about={item.about}
+						links={item.links}
+						key={nanoid()}
+					/>
+				))}
+			</div>
 		</SectionContainer>
 	)
 }
