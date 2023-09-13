5f68ea07ecb152b8d10c2bb227523597a878e35a
User <limasthays@gmail.com>
2023-09-13 17:24:06 -0300

    adds more mocked content in projects section

--- src/app/content/projectsContent.tsx
+++ src/app/content/projectsContent.tsx
@@ -33,4 +33,60 @@ export const projectsContent = [
 		about:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
 	},
+	{
+		img: Mockup,
+		projectTags: 'projeto profissional',
+		title: 'nome do projeto',
+		techs: 'next // typescript // tailwind',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
+	{
+		img: Mockup,
+		projectTags: 'projeto profissional',
+		title: 'nome do projeto',
+		techs: 'next // typescript // tailwind',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
+	{
+		img: Mockup,
+		projectTags: 'projeto profissional',
+		title: 'nome do projeto',
+		techs: 'next // typescript // tailwind',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
+	{
+		img: Mockup,
+		projectTags: 'projeto profissional',
+		title: 'nome do projeto',
+		techs: 'next // typescript // tailwind',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
 ]
