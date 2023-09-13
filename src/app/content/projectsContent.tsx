f1ea49a6f12272ae84cbe4f98f95fcc6c4ebd2a1
User <limasthays@gmail.com>
2023-09-13 15:22:30 -0300

    creates projects cards MOCKED content

---
+++ src/app/content/projectsContent.tsx
@@ -0,0 +1,36 @@
+import Mockup from '../assets/images/IMG_0930.png'
+
+export const projectsContent = [
+	{
+		img: Mockup,
+		projectTags: 'estudo de caso',
+		title: 'multistep form',
+		techs: 'next // typescript // sass',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+			{
+				title: 'github',
+				link: 'https://github.com/limasthays/sass-multistep-form',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
+	{
+		img: Mockup,
+		projectTags: 'projeto profissional',
+		title: 'nome do projeto',
+		techs: 'next // typescript // tailwind',
+		links: [
+			{
+				title: 'website',
+				link: 'https://sass-multistep-form.vercel.app/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. Proin faucibus rhoncus nisi sed posuere. Praesent congue, lorem aliquam mattis consequat, ipsum justo cursus tellus, eu varius erat ipsum non tortor. Quisque at mattis magna. Donec rutrum massa quis dui volutpat ornare. Nullam lacinia quis erat ac maximus. Suspendisse ac finibus turpis. Nulla facilisi. Integer sagittis nunc tellus, non vulputate sapien aliquam vel. Nullam ut orci ut lacus lacinia placerat id id augue. Maecenas sed commodo tellus, et placerat mi. Donec scelerisque condimentum ipsum a tempus. Nunc volutpat bibendum quam vel aliquet. Pellentesque et diam et arcu venenatis dignissim. Praesent massa mauris, iaculis sit amet lacinia at, vestibulum a massa.',
+	},
+]
