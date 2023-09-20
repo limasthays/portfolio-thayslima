321b2b7ad1d54b682f81253f771928682f92e02f
User <limasthays@gmail.com>
2023-09-19 21:11:55 -0300

    skills section: complete! rendering all content

--- src/app/components/sections/Skills.tsx
+++ src/app/components/sections/Skills.tsx
@@ -1,9 +1,25 @@
 import SectionContainer from '../containers/SectionContainer'
+import { frontSkills, anotherSkills } from '../../content/skillsContent'
+import SkillCard from '../containers/SkillCard'
 
 function SkillsSection() {
 	return (
 		<SectionContainer id="skills" sectionTitle="skills">
-			<div></div>
+			<div className="flex flex-col gap-4">
+				<h3>Tecnologias para front-end</h3>
+				<div className="grid grid-cols-3 gap-2 w-max mx-auto">
+					{frontSkills.map((item) => (
+						<SkillCard icon={item.icon} title={item.title} key={item.title} />
+					))}
+				</div>
+
+				<h3>Ferramentas adicionais</h3>
+				<div className="grid grid-cols-3 gap-2 w-max mx-auto">
+					{anotherSkills.map((item) => (
+						<SkillCard icon={item.icon} title={item.title} key={item.title} />
+					))}
+				</div>
+			</div>
 		</SectionContainer>
 	)
 }
