1adf4f06817315d1ca7689fee91cbdaf0b727639
User <limasthays@gmail.com>
2023-09-19 21:10:57 -0300

    skills content: including my skills to a file with titles and icons

---
+++ src/app/content/skillsContent.tsx
@@ -0,0 +1,43 @@
+import {
+	FaHtml5,
+	FaCss3,
+	FaJsSquare,
+	FaReact,
+	FaSass,
+	FaAngular,
+	FaUbuntu,
+} from 'react-icons/fa'
+import {
+	SiNextdotjs,
+	SiStyledcomponents,
+	SiAdobephotoshop,
+	SiAdobeillustrator,
+} from 'react-icons/si'
+import {
+	BiLogoTailwindCss,
+	BiLogoTypescript,
+	BiLogoFigma,
+	BiLogoGit,
+	BiLogoGithub,
+} from 'react-icons/bi'
+
+export const frontSkills = [
+	{ title: 'HTML', icon: <FaHtml5 /> },
+	{ title: 'CSS', icon: <FaCss3 /> },
+	{ title: 'JavaScript', icon: <FaJsSquare /> },
+	{ title: 'TypeScript', icon: <BiLogoTypescript /> },
+	{ title: 'React JS', icon: <FaReact /> },
+	{ title: 'Next JS', icon: <SiNextdotjs /> },
+	{ title: 'SASS', icon: <FaSass /> },
+	{ title: 'Tailwind', icon: <BiLogoTailwindCss /> },
+	{ title: 'Styled Components', icon: <SiStyledcomponents /> },
+]
+
+export const anotherSkills = [
+	{ title: 'Git', icon: <BiLogoGit /> },
+	{ title: 'Github', icon: <BiLogoGithub /> },
+	{ title: 'Ubuntu', icon: <FaUbuntu /> },
+	{ title: 'Figma', icon: <BiLogoFigma /> },
+	{ title: 'Adobe Photoshop', icon: <SiAdobephotoshop /> },
+	{ title: 'Adobe Illustrator', icon: <SiAdobeillustrator /> },
+]
