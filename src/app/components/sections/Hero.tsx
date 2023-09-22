b0d9a21df9de86bdeaa82312376040aaced12e39
User <limasthays@gmail.com>
2023-09-22 16:09:15 -0300

    lower opacity in texts in hero section

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -4,17 +4,18 @@ function HeroSection() {
 	return (
 		<section className="min-h-screen p-9">
 			<div className="flex flex-col gap-4">
-				<p className="font-semibold text-xl">
+				<p className="font-semibold text-xl opacity-80">
 					Olá! Eu sou
 					<span className="text-blush-pink"> Thays Lima.</span>
 				</p>
+
 				<h1 className="text-5xl">front-end developer.</h1>
 
-				<p>
+				<p className="text-sm font-semibold">
 					Desenvolvo experiências intuitivas, criativas e acessíveis para a web.
 				</p>
 
-				<p className="text-sm">
+				<p className="text-xs mt-2 opacity-80">
 					+1 ano de experiência //
 					<br />
 					habilidades em UX/UI //
