dc5964e6e0a26fb463bd16b81be041e5372d8a0d
User <limasthays@gmail.com>
2023-09-28 00:06:06 -0300

    spacing columns titles in skills section

--- src/app/components/sections/Skills.tsx
+++ src/app/components/sections/Skills.tsx
@@ -2,12 +2,16 @@ import SectionContainer from '../containers/SectionContainer'
 import { frontSkills, anotherSkills } from '../../content/skillsContent'
 import SkillCard from '../containers/SkillCard'
 
+function ColumnTitle({ title }: { title: string }) {
+	return <h3 className="mb-3 text-center">{title}</h3>
+}
+
 function SkillsSection() {
 	return (
 		<SectionContainer id="skills" sectionTitle="skills">
 			<div className="flex flex-col lg:flex-row gap-4 lg:gap-10 w-full lg:justify-center">
 				<div className="flex flex-col">
-					<h3>Tecnologias para front-end</h3>
+					<ColumnTitle title="Tecnologias para front-end" />
 					<div className="grid grid-cols-3 gap-2 w-max mx-auto">
 						{frontSkills.map((item) => (
 							<SkillCard icon={item.icon} title={item.title} key={item.title} />
@@ -16,7 +20,7 @@ function SkillsSection() {
 				</div>
 
 				<div className="flex flex-col">
-					<h3>Ferramentas adicionais</h3>
+					<ColumnTitle title="Ferramentas adicionais" />
 					<div className="grid grid-cols-3 gap-2 w-max mx-auto">
 						{anotherSkills.map((item) => (
 							<SkillCard icon={item.icon} title={item.title} key={item.title} />
