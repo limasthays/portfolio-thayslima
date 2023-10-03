b49f49172e8e8c67b3c6ca74a3ca0ef1ca62c5fe
User <limasthays@gmail.com>
2023-10-03 16:17:34 -0300

    includes projects mockups with screenshots, both on mobile and desktop view. includes real links of the projects webisites

--- src/app/content/projectsContent.tsx
+++ src/app/content/projectsContent.tsx
@@ -1,11 +1,28 @@
-import Mockup from '../assets/images/cropped_mockup.png'
+import MultistepFormMockup from '../assets/images/multistep-form-screenshot.png'
+import StackhubMockup from '../assets/images/stackhub-screenshot.png'
+import MiniBlogMockup from '../assets/images/mini-blog-screenshot.png'
+import ProximoAndarMockup from '../assets/images/proximo-andar-screenshot.png'
 
 export const projectsContent = [
 	{
-		img: Mockup,
+		img: ProximoAndarMockup,
+		projectTags: 'projeto profissional',
+		title: 'Próximo Andar',
+		techs: 'react + next // javascript + typescript // tailwind css',
+		links: [
+			{
+				title: 'website',
+				link: 'https://proximoandar.com/',
+			},
+		],
+		about:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
+	},
+	{
+		img: MultistepFormMockup,
 		projectTags: 'estudo de caso',
 		title: 'multistep form',
-		techs: 'next // typescript // sass',
+		techs: 'react + next // javascript + typescript // sass',
 		links: [
 			{
 				title: 'website',
@@ -20,70 +37,32 @@ export const projectsContent = [
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
 	},
 	{
-		img: Mockup,
-		projectTags: 'projeto profissional',
-		title: 'nome do projeto',
-		techs: 'next // typescript // tailwind',
+		img: StackhubMockup,
+		projectTags: 'estudo de caso',
+		title: 'Stackhub',
+		techs: 'react + next // javascript + typescript // tailwind css',
 		links: [
 			{
 				title: 'website',
-				link: 'https://sass-multistep-form.vercel.app/',
+				link: 'https://stackhub.vercel.app/',
+			},
+			{
+				title: 'github',
+				link: 'https://github.com/limasthays/stackhub',
 			},
 		],
 		about:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
 	},
 	{
-		img: Mockup,
-		projectTags: 'projeto profissional',
-		title: 'nome do projeto',
-		techs: 'next // typescript // tailwind',
+		img: MiniBlogMockup,
+		projectTags: 'estado de caso',
+		title: 'Mini blog',
+		techs: 'react // javascript // styled components',
 		links: [
 			{
 				title: 'website',
-				link: 'https://sass-multistep-form.vercel.app/',
-			},
-		],
-		about:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
-	},
-	{
-		img: Mockup,
-		projectTags: 'projeto profissional',
-		title: 'nome do projeto',
-		techs: 'next // typescript // tailwind',
-		links: [
-			{
-				title: 'website',
-				link: 'https://sass-multistep-form.vercel.app/',
-			},
-		],
-		about:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
-	},
-	{
-		img: Mockup,
-		projectTags: 'projeto profissional',
-		title: 'nome do projeto',
-		techs: 'next // typescript // tailwind',
-		links: [
-			{
-				title: 'website',
-				link: 'https://sass-multistep-form.vercel.app/',
-			},
-		],
-		about:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus odio diam, nec posuere enim pellentesque sed. Quisque eu rutrum erat, a ultricies leo. ',
-	},
-	{
-		img: Mockup,
-		projectTags: 'projeto profissional',
-		title: 'nome do projeto',
-		techs: 'next // typescript // tailwind',
-		links: [
-			{
-				title: 'website',
-				link: 'https://sass-multistep-form.vercel.app/',
+				link: 'https://mini-blog-nu.vercel.app/',
 			},
 		],
 		about:
